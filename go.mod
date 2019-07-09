cf1ceab851c45bb7aa011a4b0a32b58795614612
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-07-09 17:05:20 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,34 @@
+module main
+
+require (
+	github.com/Azure/go-autorest/autorest v0.3.0 // indirect
+	github.com/getsentry/sentry-go v0.1.1
+	github.com/golang/groupcache v0.0.0-20160516000752-02826c3e7903 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/go-cmp v0.3.0 // indirect
+	github.com/google/gofuzz v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.3.0 // indirect
+	github.com/gophercloud/gophercloud v0.2.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/json-iterator/go v1.1.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/sys v0.0.0-20190616124812-15dcb6c0061f // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	gopkg.in/inf.v0 v0.9.0 // indirect
+	k8s.io/api v0.0.0-20181204000039-89a74a8d264d
+	k8s.io/apimachinery v0.0.0-20181127025237-2b1284ed4c93
+	k8s.io/client-go v10.0.0+incompatible
+	k8s.io/klog v0.3.1 // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/getsentry/sentry-go"
+	api "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// get dsn
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn == "" {
+		panic("missing SENTRY_DSN")
+	}
+
+	// initialize sentry
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:        dsn,
+		Debug:      os.Getenv("SENTRY_DEBUG") == "true",
+		ServerName: "sentinel",
+		Integrations: func([]sentry.Integration) []sentry.Integration {
+			// disable all integrations
+			return nil
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// get kube master and config
+	kubeMaster := os.Getenv("KUBE_MASTER")
+	kubeConfig := os.Getenv("KUBE_CONFIG")
+
+	// prepare config
+	var config *rest.Config
+
+	// check kube master and config
+	if kubeMaster != "" || kubeConfig != "" {
+		// use provided kube master and config
+		config, err = clientcmd.BuildConfigFromFlags(kubeMaster, kubeConfig)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// otherwise get in cluster config
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// get namespace
+	namespace := os.Getenv("NAMESPACE")
+	if namespace == "" {
+		namespace = api.NamespaceAll
+	}
+
+	// create client set
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create list watch
+	listWatch := cache.NewListWatchFromClient(
+		clientSet.CoreV1().RESTClient(),
+		"events",
+		namespace,
+		fields.Everything(),
+	)
+
+	// create informer controller
+	_, controller := cache.NewInformer(
+		listWatch,
+		&api.Event{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				process(obj.(*api.Event))
+			},
+			UpdateFunc: func(_, obj interface{}) {
+				process(obj.(*api.Event))
+			},
+		},
+	)
+
+	// run controller
+	controller.Run(nil)
+}
+
+func process(event *api.Event) {
+	// prepare level
+	level := sentry.LevelInfo
+	if event.Type == api.EventTypeWarning {
+		level = sentry.LevelWarning
+	}
+
+	// prepare message
+	message := fmt.Sprintf(
+		"[%s] %s/%s: %s",
+		event.InvolvedObject.Kind,
+		event.InvolvedObject.Namespace,
+		event.InvolvedObject.Name,
+		event.Message,
+	)
+
+	// prepare sentry event
+	sentryEvent := &sentry.Event{
+		Message: message,
+		Level:   level,
+		Tags: map[string]string{
+			"type":      event.Type,
+			"reason":    event.Reason,
+			"kind":      event.InvolvedObject.Kind,
+			"name":      event.InvolvedObject.Name,
+			"namespace": event.InvolvedObject.Namespace,
+		},
+		Extra: map[string]interface{}{
+			"event":  event.Name,
+			"count":  event.Count,
+			"source": event.Source.Component,
+		},
+	}
+
+	// capture event
+	sentry.CaptureEvent(sentryEvent)
+
+	// log info
+	fmt.Printf("sent event: %s\n", message)
+}
